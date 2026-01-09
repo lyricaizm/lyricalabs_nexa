a32f1999c063e5257b9520b73242cd573631cd5d
Lyricaizm <utkutuzcuu@gmail.com>
2026-01-09 09:54:48 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import setup, find_packages
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
-with open("requirements.txt", "r") as f:
-    requirements = f.read().splitlines()
+# requirements.txt dosyasını okumak yerine direkt liste kullan
+requirements = ["requests"]
 
 setup(
     name="lyricalabs_nexa",
