eedf11dd964df1b3190f83fbaa560f9b3632089b
Lyricaizm <utkutuzcuu@gmail.com>
2026-01-09 10:34:44 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ requirements = ["requests"]
 
 setup(
     name="lyricalabs-nexa",
-    version="0.3.4",
+    version="0.3.5",
     author="Lyrica Labs",
     author_email="lyricalabs@gmail.com",
     description="Lyrica Labs Nexa LLM API Python İstemci Kütüphanesi",
