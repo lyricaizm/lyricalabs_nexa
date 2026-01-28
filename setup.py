b372d972c18442dcaa764121672611c42e556f68
Lyricaizm <utkutuzcuu@gmail.com>
2026-01-28 18:21:07 +0300

    Update package details for XanaxWay API

--- setup.py
+++ setup.py
@@ -7,22 +7,22 @@ def read_readme():
         with open('README.md', 'r', encoding='utf-8') as f:
             return f.read()
     except FileNotFoundError:
-        return "Lyrica Labs API Python, Nexa LLM"
+        return "XanaxWay API Python, Nexa LLM"
 
 long_description = read_readme()
 
 setup(
-    name="lyricalabs",
+    name="xanaxway",
     version="0.1.3",
     packages=find_packages(),
     install_requires=["requests"],
     python_requires=">=3.8",
-    description="Lyrica Labs API Python, Nexa LLM",
+    description="XanaxWay API Python, Nexa LLM",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://lyricalabs.com",
-    author="Lyrica Labs",
-    author_email="lyricalabs@gmail.com",
+    author="XanaxWay",
+    author_email="xanaxway@gmail.com",
     classifiers=[
         "Programming Language :: Python :: 3",
         "Operating System :: OS Independent",
